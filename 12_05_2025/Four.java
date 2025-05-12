76eb063a7fa02e72815f7839e36e4d6aaab39815
Varma <narishettinagaraju26@gmail.com>
2025-05-12 14:30:53 +0530

    initail commit

--- 12_05_2025/Four.java
+++ 12_05_2025/Four.java
@@ -1,7 +1,18 @@
+/*
+ * list 2,3,4,3,2,5,5
+ * every number has freq 2 only 1  number will have once
+ */
 import java.util.*;
 public class Four { 
     public static void main(String[] args) throws Exception {
     Scanner sc=new Scanner(System.in);
-        
+    // int n=sc.nextInt();
+    ArrayList<Integer> list= new ArrayList<>(Arrays.asList(2,3,4,3,2,5,5));
+    int ans = list.get(0);
+    for(int i=1;i<list.size();i++){
+        ans^=list.get(i);
+    }
+    System.out.println(ans);
+    // System.out.println(2^3^4^3^2^5^5);
     }
 }
